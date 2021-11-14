dfd357052d2538121aef28985f597a44a835e05e
ktompoidi <katya.tompoidi@onior.com>
2021-11-14 14:50:05 +0100

    updated test

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -1,4 +1,6 @@
 import pickle
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 from main import clf
 
 
@@ -16,3 +18,13 @@ def test_accuracy():
 
     # Accuracy should be over 90%
     assert acc > 0.95
+
+
+def test_pipeline_and_scaler():
+    isPipeline = isinstance(clf,Pipeline)
+    assert isPipeline
+
+    if isPipeline:
+        # check that the first step in the pipeline is scaling
+        first_step = [v for v in clf.named_steps.values()[0]]
+        assert isinstance(first_step, StandardScaler)
